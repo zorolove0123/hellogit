0bb3197c1629e9412084d29f92dbb2780cf4d45a
zorolove0123 <gy15lmt@leeds.ac.uk>
2016-03-16 16:44:53 +0000

    Start

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+package uk.ac.leeds.geog.pgiaas.hellogit;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+	
+		System.out.println("Hi");
+		
+	}
+	
+	public static void main (String args[]) {
+	
+		new HelloWorld();
+		
+	}
+	
+}
